5a698ee15e222b9361620553d666cddaae929a50
Timon Owuor <infotechhaven6.com~>
2025-08-15 14:13:30 +0300

    update

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import connectDB from './config/db.js';
 import checkoutRoutes from './routes/checkout.js';
 import recruitmentRoutes from './routes/recruitment.js'; // Assuming you have a recruitment route
 import expertConnectRoutes from "./routes/expertConnect.js";
-import marketLinkRoutes from "./routes/marketLink.js"; // Assuming you have a market link route
+import marketLinkRoutes from "./routes/marketLink.js";
 
 dotenv.config();
 const app = express();
